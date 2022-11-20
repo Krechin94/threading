b45c51b5f56c5b8e5b8f4c43c86413b01d58f8c5
Krechin94 <andreikrech@gmail.com>
2022-11-21 02:51:48 +0300

    add new project to learn threading

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nikitqa loh");
+        }
+    }
+}
